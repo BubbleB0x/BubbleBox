f53a223a214c2f1f5f4862939bc01e527df63804
Andrea Polenta <andreapolenta@MBP-di-Andrea.fritz.box>
2020-04-04 01:03:27 +0200

    complete authentication

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,16 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Plugins } from '@capacitor/core';
+import { Router } from '@angular/router';
+const { Storage } = Plugins;
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
 
-  constructor() {
+  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {
   }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.loginForm.value).subscribe(
+
+      (response) => {
+
+
+        Storage.set({
+          key: 'apiKey',
+          value: response.api_key
+        });
+
+        Storage.set({
+          key: 'name',
+          value: response.name
+        });
+
+        Storage.set({
+          key: 'id',
+          value: response.id
+        });
+
+        Storage.set({
+          key: 'reqKey',
+          value: btoa(response.id + ':' + response.name)
+        });
+
+        this.loginForm.reset();
+        this.router.navigate(['/home']);
+      },
+
+      (error) => {
+        console.log(error);
+      });
   }
 
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../../environments/environment';
+import { HttpHeaders } from '@angular/common/http';
+import { Plugins } from '@capacitor/core';
+import { ApiKey } from 'src/app/interfaces/apiKey/api-key';
+import { Router } from '@angular/router';
+const { Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(cred) {
+    return this.http.post<ApiKey>(environment.apiUrl + '_security/api_key', {
+      "name": "my-api-key"
+    }, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + btoa(cred.username + ':' + cred.password)
+      })
+    })
+  }
 }
